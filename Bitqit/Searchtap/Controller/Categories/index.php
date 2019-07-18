f7c3b6e5b476bbae05e75d7c065e3d23dfa80211
praveen <praveen@bitqit.com>
2019-07-18 17:29:30 +0530

    Updated file for route

--- Bitqit/Searchtap/Controller/Categories/index.php
+++ Bitqit/Searchtap/Controller/Categories/index.php
@@ -7,17 +7,32 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     private $conf;
     private $stcurl;
-    public function __construct(\Bitqit\Searchtap\Helper\getConfigValue $config)
+    public function __construct(\Bitqit\Searchtap\Helper\getConfigValue $config,
+                                \Magento\Framework\App\Action\Context $context,
+                                \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
     {
         $this->conf=$config;
+        parent::__construct($context);
     }
 
     public function execute()
     {
-
        $this->stcurl = new Helper\searchtapCurl($this->conf->applicationId, $this->conf->collectionName, $this->conf->adminKey);
-       $storeId=$this->getRequest()->getParam('sid');
-       $this->categoryJson($storeId);
+       $storeId=$this->getRequest()->getParam('sid',0);
+       $categoriesIds=$this->getRequest()->getParam('catId',0);
+       if($storeId)
+       {
+           if(!($categoriesIds)){
+               $this->categoryJson($storeId);
+           }
+           else{
+               $this->commaSeperatedIds($storeId,$categoriesIds);
+           }
+       }
+       else
+       {
+           echo "Please specify Store Id using (sid=1/2/3/..)";
+       }
 
     }
 
@@ -45,12 +60,12 @@ class Index extends \Magento\Framework\App\Action\Action
                  $pathIds = explode('/', $category->getPath());
                  foreach ($pathIds as $path){
                  $collection = $categoryFactory->create()->setStoreId($sid)->addAttributeToSelect('name')->addFieldToFilter('entity_id', array('in' => $path));
-                 $pahtByName = '';
+                 $pathByName = '';
 
                  foreach ($collection as $cat) {
-                        $pahtByName .= $cat->getName();
+                     $pathByName .= $cat->getName();
                  }
-                     $pathArray[] = $pahtByName;
+                     $pathArray[] = $pathByName;
                  }
 
             $path=implode('|||',$pathArray);
@@ -78,9 +93,25 @@ class Index extends \Magento\Framework\App\Action\Action
         }
 
         $categoryJson=json_encode($categoryArray);
-        //print_r($categoryJson);
-        $response=$this->stcurl->searchtapCurlRequest($categoryJson);
+        print_r($categoryJson);
+      //  $response=$this->stcurl->searchtapCurlRequest($categoryJson);
         //echo $response;
-    } 
+        return true;
+    }
+
+
+
+    public function commaSeperatedIds($storeid,$categoryids){
+
+        $categoryidsIds = explode(",", $categoryids);
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $categoryFactory = $objectManager->create('Magento\Catalog\Model\ResourceModel\Category\CollectionFactory');
+        $categories = $categoryFactory->create()->addAttributeToSelect('*')->setStore($storeid)->addAttributeToFilter('entity_id', array('in' => $categoryids));
+        foreach ($categories as $cat)
+        {
+            echo $cat->getName();
+        }
+       return true;
+    }
 
 }
