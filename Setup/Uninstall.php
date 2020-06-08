3afe6725f8106b1bac8e2d31ba27fb8e90d3f9fc
praveenmaurya58 <praveen@bitqit.com>
2020-06-08 13:36:57 +0530

    Updated

---
+++ Setup/Uninstall.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bitqit\Searchtap\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class Uninstall implements UninstallInterface
+{
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        $connection = $installer->getConnection();
+        $connection->dropTable('searchtap_queue');
+        $connection->dropTable('searchtap_config');
+        
+        $installer->endSetup();
+    }
+}
