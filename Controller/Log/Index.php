d317e855cbc44c1a72cf304ac75168654ef16186
Ravneet Kaur <ravneet@bitqit.com>
2020-09-08 21:36:35 +0530

    Added token validation and resolved log file path issue.
    Added token validation and resolved log file path issue.

--- Controller/Log/Index.php
+++ Controller/Log/Index.php
@@ -1,28 +1,42 @@
 <?php
 
+
 namespace Bitqit\Searchtap\Controller\Log;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+   private $logFileName = "/var/log/searchtap.log";
+   private $dataHelper;
 
-    private $logFileName = "./var/log/searchtap.log";
-    public function __construct(
+   public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Bitqit\Searchtap\Helper\SearchtapHelper $searchtapHelper
+        \Bitqit\Searchtap\Helper\SearchtapHelper $searchtapHelper,
+        \Bitqit\Searchtap\Helper\Data $dataHelper
     )
     {
-        $this->searchtapHelper = $searchtapHelper;
+     	$this->searchtapHelper = $searchtapHelper;
+        $this->dataHelper = $dataHelper;
         parent::__construct($context);
     }
+    
     public function execute()
     {
-        try {
-            $logs = file_get_contents($this->logFileName);
+     	try {
+            $token = $this->getRequest()->getParam('token');
+
+            if (!$this->dataHelper->checkPrivateKey($token)) {
+              $this->getResponse()->setStatusCode($this->searchtapHelper->getStatusCodeList()[403]);
+              $this->getResponse()->setBody('Unauthorized Access');
+              return;
+            }
+
+            $logs = file_get_contents(BP . $this->logFileName);
             $this->getResponse()->setHeader('content-type', 'application/json');
             $this->getResponse()->setStatusCode($this->searchtapHelper->getStatusCodeList()[200]);
             $this->getResponse()->setBody($logs);
         } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
+             var_dump($e->getMessage());
         }
     }
 }
+
